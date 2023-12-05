2e559a2b5661240fc708ac7566f2223179fd32c4
Baranceanu,Andrei <andrei.baranceanu@dentsplysirona.com>
2023-12-05 16:40:10 +0200

    some things

--- lib/world_time/main.dart
+++ lib/world_time/main.dart
@@ -5,15 +5,13 @@ import 'package:flutter_net_ninja_tutorial/world_time/pages/choose_location.dart
 import 'package:flutter_net_ninja_tutorial/world_time/pages/home.dart';
 import 'package:flutter_net_ninja_tutorial/world_time/pages/loading.dart';
 
-
 void main() => runApp(MaterialApp(
-  // used for the moment, while we create the home screen
-  initialRoute: '/home',
-    routes: {
-      // this defines the base route; should be the same as the value of the home:
-      '/': (context) => Loading(),
-      '/home': (context) => Home(),
-      '/location': (context) => ChooseLocation(),
-    },
+      // used for the moment, while we create the home screen
+      initialRoute: '/',
+      routes: {
+        // this defines the base route; should be the same as the value of the home:
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation(),
+      },
     ));
-
--- lib/world_time/pages/loading.dart
+++ lib/world_time/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,9 +10,28 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+//   void getData() async {
+  // in a Response type variable store the response given by the API
+//     Response response =
+  // sintax to parse a string to an Uri so that we get the response
+//         await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+  // save in a Map type variable the decoded response
+  // the response comes in a String format - it has to be converted to be usable
+//     Map data = jsonDecode(response.body);
+//     print(data);
+//     print(data['title']);
+//   }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Text('loading'),
     );
   }
