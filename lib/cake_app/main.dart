18607ba28c1fcb716ccd5964156635adfca0025f
ciprian.tocu <ciprian.tocu@heavensolutions.com>
2023-12-12 09:57:32 +0200

    an attempt

--- lib/cake_app/main.dart
+++ lib/cake_app/main.dart
@@ -5,6 +5,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_net_ninja_tutorial/quotes/quote_card.dart';
 import 'package:flutter_net_ninja_tutorial/cake_app/utils.dart';
+import 'package:file_picker/file_picker.dart';
+
+import 'recipe_card.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:path/path.dart';
 
 void main() => runApp(MaterialApp(
       home: RecipeList(),
@@ -18,16 +24,49 @@ class RecipeList extends StatefulWidget {
 }
 
 class _RecipeListState extends State<RecipeList> {
-  List <List<dynamic>> fileWithRecipes = [];
+  List<List<dynamic>> fileWithRecipes = [];
 
   void _loadCSV() async {
-     final fileCSV = await rootBundle.loadString("assets/book2.csv");
-     List<List<dynamic>> listData = 
-     const CsvToListConverter().convert(fileCSV);
-     setState(() {
-      fileWithRecipes = listData;
-     });
+    var csvFile = await rootBundle.loadString("assets/book2.csv");
+    List<List<dynamic>> csvData = const CsvToListConverter().convert(csvFile);
+    setState(() {
+      fileWithRecipes = csvData;
+    });
   }
+
+  generateCsv() async {
+    var csvFile = await rootBundle.loadString("assets/book2.csv");
+    List<List<dynamic>> csvFileList =
+        const CsvToListConverter().convert(csvFile);
+    String csvData = ListToCsvConverter().convert(csvFileList);
+    final path = "assets/book1.csv";
+    final File file = File(path);
+    await file.writeAsString(csvData);
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return LoadCsvDataScreen(path: path);
+        },
+      ),
+    );
+  }
+
+  LoadCsvFromStorate() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      allowedExtensions: ['csv'],
+      type: FileType.custom,
+    );
+    String? path = result?.files.first.path;
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return LoadCsvDataScreen(path: path);
+        },
+      ),
+    );
+  }
+
+  Future<List<List<dynamic>>> loadingCsvData
 
   @override
   Widget build(BuildContext context) {
@@ -44,18 +83,17 @@ class _RecipeListState extends State<RecipeList> {
         backgroundColor: Colors.amber,
       ),
       body: ListView.builder(
-        itemCount: fileWithRecipes.length,
-        itemBuilder: (_, index){
-        // in recipe save the first entry from fileWithRecipes after we split it
-        // fileWithRecipes[index] == each row from the csv file
-        // fileWithRecipes[index][0] == in the first place, we get the recipe as a 
-        // solo string with index 0; therefore we split it into individual elements
-        final recipe = fileWithRecipes[index][0].split(';');
-        // call the RecipeCard constructor which returns a Card with info regarding
-        // each recipe
-        return RecipeCard(recipe: recipe, editRecipe: (){});
-        }
-      ),
+          itemCount: fileWithRecipes.length,
+          itemBuilder: (_, index) {
+            // in 'recipe' save the first entry from fileWithRecipes after we split it
+            // fileWithRecipes[index] == each row from the csv file
+            // fileWithRecipes[index][0] == in the first place, we get the recipe as a
+            // solo string with index 0; therefore we split it into individual elements
+            final recipe = fileWithRecipes[index][0].split(';');
+            // call the RecipeCard constructor which returns a Card with info regarding
+            // each recipe
+            return RecipeCard(recipe: recipe, editRecipe: () {});
+          }),
     );
   }
 }
